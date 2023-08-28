f30e8202d2ab8471dd60e4790ea1f5705e872eff
ntraun <Theeledo4@gmail.com>
2023-08-28 15:59:04 +0200

    added some parsing element

--- headers/Utils.hpp
+++ headers/Utils.hpp
@@ -1,23 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Utils.cpp                                          :+:      :+:    :+:   */
+/*   Utils.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ntraun <ntraun@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/28 15:56:46 by ntraun            #+#    #+#             */
-/*   Updated: 2023/08/28 15:58:47 by ntraun           ###   ########.fr       */
+/*   Created: 2023/08/28 15:53:25 by ntraun            #+#    #+#             */
+/*   Updated: 2023/08/28 15:58:27 by ntraun           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Utils.hpp"
+#pragma once
+
+#include "Main.hpp"
 
 namespace utils
 {
-	void	ParseArgs(int argc, char **argv)
-	{
-		(void)argv;
-		if (argc != 3)
-			throw std::invalid_argument("Invalid number of arguments");
-	}
+	void	ParseArgs(int argc, char **argv);
 }
--- sources/main.cpp
+++ sources/main.cpp
@@ -6,16 +6,25 @@
 /*   By: ntraun <ntraun@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 15:04:18 by ntraun            #+#    #+#             */
-/*   Updated: 2023/08/22 15:06:42 by ntraun           ###   ########.fr       */
+/*   Updated: 2023/08/28 15:57:38 by ntraun           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
+#include "Main.hpp"
 
 int	main(int argc, char **argv)
 {
 	(void)argc;
 	(void)argv;
+	try
+	{
+		utils::ParseArgs(argc, argv);
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+		return (1);
+	}
 	std::cout << "Hello World!" << std::endl;
 	return (0);
 }
