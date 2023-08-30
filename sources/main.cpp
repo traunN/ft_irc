a5bf1b41272e8a3a653ca9897dc38b36eebc93a3
ntraun <Theeledo4@gmail.com>
2023-08-30 14:48:14 +0200

    ALL ON MAIN NOW WE SPLIT IT ? LOL

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,12 +1,126 @@
 #include "Main.hpp"
 #include <netdb.h>
+#include <fcntl.h>
+#include <algorithm>
 
-#define MY_PORT "8080"
-#define BACKLOG 10
+#define PORT 8080
 
-int	main(int argc, char **argv)
+int main(int argc, char const* argv[])
 {
-	// try {
+	(void)argc;
+	(void)argv;
+	int server_fd, new_socket, valread;
+	struct sockaddr_in address;
+	int opt = 1;
+	int addrlen = sizeof(address);
+	char buffer[1024] = { 0 };
+
+	// Creating socket file descriptor
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		perror("socket failed");
+		exit(EXIT_FAILURE);
+	}
+	int flags = fcntl(server_fd, F_GETFL, 0);
+	if (flags == -1) {
+		perror("fcntl");
+		exit(EXIT_FAILURE);
+	}
+	if (fcntl(server_fd, F_SETFL, flags | O_NONBLOCK) == -1) {
+		perror("fcntl");
+		exit(EXIT_FAILURE);
+	}
+
+	// Forcefully attaching socket to the port 8080
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+		perror("setsockopt");
+		exit(EXIT_FAILURE);
+	}
+	address.sin_family = AF_INET;	
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons(PORT);
+
+	// Forcefully attaching socket to the port 8080
+	if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+		perror("bind failed");
+		exit(EXIT_FAILURE);
+	}
+	if (listen(server_fd, 3) < 0) {
+		perror("listen");
+		exit(EXIT_FAILURE);
+	}
+	  int max_fd = server_fd;
+    std::vector<int> client_sockets; // Vector to store the connected client sockets
+    while (true) {
+		fd_set readfds;
+		FD_ZERO(&readfds);
+		FD_SET(server_fd, &readfds);
+		for (std::vector<int>::iterator it = client_sockets.begin(); it != client_sockets.end(); ++it) {
+			FD_SET(*it, &readfds);
+		}
+        // Use select to monitor file descriptors for activity
+        if (select(max_fd + 1, &readfds, NULL, NULL, NULL) < 0) {
+			perror("select");
+			exit(EXIT_FAILURE);
+		}
+		
+        // Check for activity on file descriptors
+        for (int fd = 0; fd <= max_fd; fd++) {
+            if (FD_ISSET(fd, &readfds)) {
+                // Handle activity on the file descriptor
+                if (fd == server_fd) {
+                    // New client connection
+                    if ((new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen)) < 0) {
+                        perror("accept");
+                        exit(EXIT_FAILURE);
+                    }
+                    std::cout << "New user connected" << std::endl;
+                    valread = recv(new_socket, buffer, 1024, 0);
+					std::cout << buffer;
+					bzero(buffer, 1024);
+                    // send(new_socket, buffer, 1024, 0);
+                    // Add the new client socket to the set and vector
+                    FD_SET(new_socket, &readfds);
+                    client_sockets.push_back(new_socket);
+
+                    // Update the maximum file descriptor value
+                    if (new_socket > max_fd) {
+                        max_fd = new_socket;
+                    }
+                } else {
+                    // Activity from an existing client
+                    // Handle the client's request
+                    valread = recv(fd, buffer, 1024, 0);
+					if (valread > 0) {
+						// Data received
+						std::cout << buffer;
+						bzero(buffer, 1024);
+						// Handle client request here
+						// Example: Send response back to the client
+						// send(fd, buffer, strlen(buffer) + 1, 0);
+					} else if (valread == 0) {
+						// Connection closed by the client
+						std::cout << "Connection closed by the client" << std::endl;
+						// Close the socket and remove it from the set and vector
+						close(fd);
+						FD_CLR(fd, &readfds);
+						client_sockets.erase(std::remove(client_sockets.begin(), client_sockets.end(), fd), client_sockets.end());
+					}
+            	}
+        	}
+    	}
+	}
+    // Close all the client sockets
+    for (int i = 0; i < sizeof(client_sockets) / sizeof(client_sockets[0]); i++) {
+    	close(client_sockets[i]);
+	}
+
+    // closing the listening socket
+    close(server_fd);
+
+    return 0;
+}
+
+// try {
 	// 	utils::ParseArgs(argc, argv);
 	// }
 	// catch (std::exception &e) {
@@ -39,26 +153,3 @@ int	main(int argc, char **argv)
 	// 	std::cerr << e.what() << std::endl;
 	// 	return (1);
 	// }
-	(void)argc;
-	(void)argv;
-	struct sockaddr_storage	their_addr;
-	socklen_t	addr_size;
-	struct addrinfo hints, *res;
-	int	sockfd, new_fd;
-
-	memset(&hints, 0, sizeof hints);
-	hints.ai_family = AF_UNSPEC; // use IPv4 or IPv6, whichever
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags = AI_PASSIVE; // fill in my IP for me
-
-	getaddrinfo(NULL, MY_PORT, &hints, &res);
-	// make a socket, bind it, and listen on it:
-	sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-	bind(sockfd, res->ai_addr, res->ai_addrlen);
-	listen(sockfd, BACKLOG);
-	std::cout << "Server fd is " << sockfd << std::endl;
-	addr_size = sizeof their_addr;
-	new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size);
-	std::cout << "New fd is " << new_fd << std::endl;
-	return (0);
-}
