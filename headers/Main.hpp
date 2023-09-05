c6e6eeababf78e597c0293813cf9bab5dbd9b1f1
mjouot <mjouot@student.42angouleme.fr>
2023-09-05 15:54:24 +0200

    tmp

--- headers/Main.hpp
+++ headers/Main.hpp
@@ -10,7 +10,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <openssl/sha.h>
+//#include <openssl/sha.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
